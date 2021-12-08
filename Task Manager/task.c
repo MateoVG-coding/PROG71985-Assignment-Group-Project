80072dfb053a6ef70ab046bead0cb7213ae1fee4
MateoVG-coding <mateovega10@hotmail.co>
2021-12-07 23:01:02 -0500

    Added createArrayTasks function, also added setters and getters functions for the structure task

--- Task Manager/task.c
+++ Task Manager/task.c
@@ -2,3 +2,41 @@
 //Mateo Vega & Jose Ibanez - December 2021
 
 #include "task.h"
+
+void createArrayTasks(PTASK tasks[])
+{
+	for (int i = 0; i < NUMBEROFTASKS; i++)
+	{
+		tasks[i] = (PTASK)malloc(sizeof(TASK));
+	}
+}
+
+void setTaskNumber(PTASK t, int taskNumber)
+{
+	t->taskNumber = taskNumber;
+}
+
+void setStatus(PTASK t, char status[])
+{
+	strncpy_s(t->status, LENGTHSIZE, status, LENGTHSIZE);
+}
+
+void setDescription(PTASK t, char description[])
+{
+	strncpy_s(t->description, LENGTHDESCRIPTION, description, LENGTHDESCRIPTION);
+}
+
+int getTaskNumber(TASK t)
+{
+	return (t.taskNumber);
+}
+
+char* getStatus(TASK t)
+{
+	return (t.status);
+}
+
+char* getDescription(TASK t)
+{
+	return (t.description);
+}
--- Task Manager/task.h
+++ Task Manager/task.h
@@ -4,10 +4,13 @@
 #pragma once
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 #define LENGTHSIZE 10
 #define LENGTHDESCRIPTION 40
+#define NUMBEROFTASKS 30
 
 typedef struct task
 {
@@ -16,3 +19,11 @@ typedef struct task
 	char description[LENGTHDESCRIPTION];
 
 }TASK, * PTASK;
+
+void createArrayTasks(PTASK[]);
+
+void setTaskNumber(PTASK, int);
+
+void setStatus(PTASK, char[]);
+
+void setDescription(PTASK, char[]);
